486697e417c0897196d3dbda8cfde7ccd738dca1
hitcodemann <89034654+hitcodemann@users.noreply.github.com>
2023-06-09 17:33:38 +0530

    30)Delete Node In A Linked List.cpp

---
+++ 30)Delete Node In A Linked List.cpp
@@ -0,0 +1,6 @@
+void deleteNode(LinkedListNode<int> * node) {
+      node->data = node->next->data;
+    LinkedListNode<int> *temp = node->next;
+    node->next=node->next->next;
+    delete temp;
+}
