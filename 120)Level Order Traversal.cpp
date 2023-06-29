96b79ba4fca6ac1eb1be349698514aa4feafd194
hitcodemann <89034654+hitcodemann@users.noreply.github.com>
2023-06-29 09:33:02 +0530

    120)Level Order Traversal.cpp

---
+++ 120)Level Order Traversal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+void getLevelOrder(BinaryTreeNode<int> *root, vector<int> &ans){
+    if(root == NULL){
+        return;
+    }
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);    
+    while(!q.empty()){
+        BinaryTreeNode<int> *front = q.front();
+        q.pop();
+
+        ans.push_back(front->val);
+
+        if (front->left != NULL){
+            q.push(front->left);
+        }
+        if(front->right != NULL){
+            q.push(front->right);
+        }
+    }
+
+}
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+    vector<int> ans;
+    getLevelOrder(root, ans);
+    return ans;
+}
